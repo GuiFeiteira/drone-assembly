10e1933b3246b562f68d5a5fbc894a1fae66876d
TheNeves13 <pedroneves.box@gmail.com>
2024-05-20 15:34:46 +0100

    Start Project

---
+++ src/controllers/droneController.js
@@ -0,0 +1,41 @@
+const Drone = require('../models/Drone');
+
+exports.createDrone = async (req, res) => {
+  const { name, type } = req.body;
+  const userId = req.user.id; // Assume that req.user is set by a middleware after token verification
+  try {
+    const drone = await Drone.create({ name, type, user: userId });
+    res.status(201).json(drone);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
+
+exports.getDrones = async (req, res) => {
+  try {
+    const drones = await Drone.find({ user: req.user.id });
+    res.json(drones);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
+
+exports.updateDrone = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const drone = await Drone.findByIdAndUpdate(id, req.body, { new: true });
+    res.json(drone);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
+
+exports.deleteDrone = async (req, res) => {
+  const { id } = req.params;
+  try {
+    await Drone.findByIdAndDelete(id);
+    res.json({ message: 'Drone deleted' });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
---
+++ src/index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const swaggerUi = require('swagger-ui-express');
+const swaggerJsdoc = require('swagger-jsdoc');
+const swaggerOptions = require('./docs/swaggerOptions');
+
+const app = express();
+
+// Middleware
+app.use(express.json());
+
+// Swagger setup
+const specs = swaggerJsdoc(swaggerOptions);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs));
+
+// Routes
+const userRoutes = require('./routes/users');
+const droneRoutes = require('./routes/drones');
+
+app.use('/api/users', userRoutes);
+app.use('/api/drones', droneRoutes);
+
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect('mongodb://localhost/droneAssembly', { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log('Connected to MongoDB');
+    app.listen(PORT, () => console.log('Server running on port ${PORT}'));
+  })
+  .catch(err => console.error('Could not connect to MongoDB', err));
