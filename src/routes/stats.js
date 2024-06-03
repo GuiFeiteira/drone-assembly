0f36603ae8274ba9dd47804272efb986de9b3d77
TheNeves13 <pedroneves.box@gmail.com>
2024-06-03 22:02:14 +0100

    finally

--- src/routes/stats.js
+++ src/routes/stats.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const estatisticasController = require('../controllers/estatisticasController');
-const authenticateJWT = require('../middleware/auth');
+const authenticateJWT = require('../midlleware/authMiddle');
 
 // Rota protegida para obter estatísticas (requer autenticação)
 router.get('/', authenticateJWT, estatisticasController.getStats);
